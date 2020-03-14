bf39da38739b96063806a180adc4da65b5dc970b
Laxus <lyphatdat1996@gmail.com>
2020-03-14 19:31:54 +0700

    model done

---
+++ TeduShop.Model/Models/PostCategory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("PostCategry")]
+    public class PostCategory : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+        [Required]
+        [MaxLength(256)]
+        public string Name { set; get; }
+        [Column(TypeName = "varchar")]
+        [Required]
+        [MaxLength(256)]
+        public string Alias { set; get; }
+        [MaxLength(500)]
+        public string Description { set; get; }
+        public int? ParentID { set; get; }
+        public int? DisplayOrder { set; get; }
+        [MaxLength(256)]
+        public string Image { set; get; }
+
+        public virtual IEnumerable<Post> Post { set; get; }
+    }
+}
---
+++ TeduShop.Model/Models/ProductTag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("ProductTag")]
+    public class ProductTag
+    {
+        public int ProductID { set; get; }
+        public string TagID { set; get; }
+
+        [ForeignKey("ProductID")]
+        public virtual Product Product { set; get; }
+        [ForeignKey("TagID")]
+        public virtual Tag Tag { set; get; }
+    }
+}
